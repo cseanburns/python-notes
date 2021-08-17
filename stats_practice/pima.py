96018ed8492970ce3ac273c3cb23a4ca045895a3
C. Sean Burns <sean.csb@gmail.com>
2021-08-17 09:46:53 -0400

    added some comments about ()

--- stats_practice/pima.py
+++ stats_practice/pima.py
@@ -2,10 +2,34 @@
 
 import pandas as pd
 
-# Importing data from URL
+"""
+Importing data from URL. Note that since we're using 'pd.read_csv', the data
+will automatically be a pandas.DataFrame
+"""
 pima = pd.read_csv('http://heather.cs.ucdavis.edu/FasteR/data/Pima.csv', header = 0)
 
-# cf. to R's dim(df)
+"""
+Cf. ``df.shape`` to R's ``dim(df)``
+Note on when to use paranthesis
+
+We write pima.shape and not pima.shape() because DataFrame.shape is a **property**
+
+See the documentation at
+https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.shape.html
+
+At the top of the page, the documentation is helpful here in that it states:
+
+*property* DataFrame.shape
+
+Compare that to something like a method, such as DataFrame.bool()
+Also, try ``help(DataFrame.bool`` in the Python IDLE
+
+https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.bool.html
+
+Near the top of the page, the document refers to:
+
+DataFrame.bool()
+"""
 pima.shape # equiv to R's dim(df)
 
 # cf. to R table()
